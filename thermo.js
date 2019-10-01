74a24651fe99f381d2f3e465d59f96b1a57d6182
Casey Harkins <caseyharkins@gmail.com>
2019-10-01 08:53:28 -0500

    Add missing thermo.js/html for supporting THERMOplate.
    These files were published to npm, but do not appear to have
    been committed to the git repo.

---
+++ thermo.js
@@ -0,0 +1,22 @@
+module.exports = function (RED) {
+    function THERMONode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.plate = RED.nodes.getNode(config.config_plate).plate;
+        node.channel = parseInt(config.channel, 10);
+        node.temperature = 0;
+        node.on('input', function (msg) {
+            const obj = {cmd: "getTEMP", args: {channel: node.channel}};
+            node.plate.send(obj, (reply) => {
+                node.temperature = reply.value;
+                node.status({text: node.temperature});
+                node.send({payload: node.temperature});
+            });
+        });
+
+        this.on('close', function () {
+            // cleanup goes here
+        });
+    }
+    RED.nodes.registerType("ppTHERMO", THERMONode);
+}
