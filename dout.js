bfd5ab7ee4b49cf33cf44b9bacd6ed3610493e53
Tyler Stowell <tyler@harschsystems.com>
2020-06-02 20:23:18 -0600

    Error handling for DIN and DOUT

--- dout.js
+++ dout.js
@@ -3,21 +3,38 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         this.plate = RED.nodes.getNode(config.config_plate).plate;
         this.output = parseInt(config.output, 10);
+        const verifier = {cmd: "VERIFY", args: {}};
+        this.plate.send(verifier, (reply) => {
+            var type = RED.nodes.getNode(config.config_plate).model;
+            if (reply.state == 1 && ((type == "DAQCplate" || type == "DAQC2plate") && this.output < 7 || type == "TINKERplate" && this.output > 0)){
+                this.status({fill: "green", shape: "ring", text: "plate validated"});
+                this.verified = true;
+                const conf = {cmd: "setOUT", args: {bit: this.output}};
+                this.plate.send(conf, (reply) => {});
+            }else{
+                this.status({fill: "red", shape: "ring", text: "invalid plate or input"});
+                this.verified = false;
+            }
+        });
         var node = this;
         node.on('input', function (msg) {
-            const obj = {args: {bit: node.output}};
-            if (msg.payload == "on") {
-                obj['cmd'] = "setDOUTbit";
-            } else if (msg.payload == "off") {
-                obj['cmd'] = "clrDOUTbit";
-            } else if (msg.payload == "toggle") {
-                obj['cmd'] = "toggleDOUTbit";
+            if (node.verified){
+                const obj = {args: {bit: node.output}};
+                if (msg.payload == "on") {
+                    obj['cmd'] = "setDOUTbit";
+                } else if (msg.payload == "off") {
+                    obj['cmd'] = "clrDOUTbit";
+                } else if (msg.payload == "toggle") {
+                    obj['cmd'] = "toggleDOUTbit";
+                }
+                node.plate.send(obj, (reply) => {
+                    node.state = reply.state
+                    node.status({text: node.state});
+                    node.send({payload: node.state});
+                });
+            }else{
+                //TODO: Handle invalid message
             }
-            node.plate.send(obj, (reply) => {
-                node.state = reply.state
-                node.status({text: node.state});
-                node.send({payload: node.state});
-            });
         });
 
         this.on('close', function () {
