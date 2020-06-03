3ed311832ba121f3a65c8f249f39d928a7f421ac
Tyler Stowell <tyler@harschsystems.com>
2020-06-03 11:39:53 -0600

    Implemented safety checks for ADC, DAC, Relay, and LED control

--- dac.js
+++ dac.js
@@ -4,19 +4,34 @@ module.exports = function (RED) {
         this.plate = RED.nodes.getNode(config.config_plate).plate;
         this.channel = parseInt(config.channel, 10);
         this.voltage = 0;
+        const verifier = {cmd: "VERIFY", args: {}};
+        this.plate.send(verifier, (reply) => {
+            var type = RED.nodes.getNode(config.config_plate).model;
+            if (reply.state == 1 && (type == "DAQCplate" && this.channel < 2 || type == "DAQC2plate")){
+                this.status({fill: "green", shape: "ring", text: "plate validated"});
+                this.verified = true;
+            }else{
+                this.status({fill: "red", shape: "ring", text: "invalid plate or input"});
+                this.verified = false;
+            }
+        });
         var node = this;
         node.on('input', function (msg) {
-            if (typeof msg.payload == "number" &&
-                (msg.payload >= 0) &&
-                (msg.payload <= 4.095)) {
-                const obj = {cmd: "setDAC", args: {channel: node.channel, value: msg.payload}};
-                node.plate.send(obj, (reply) => {
-                    node.value = reply.value;
-                    //node.status({text: node.value});
-                    node.send({payload: node.value});
-                });
-            } else {
-                this.warn("invalid DAC value: ignoring");
+            if(node.verified){
+                if (typeof msg.payload == "number" &&
+                    (msg.payload >= 0) &&
+                    (msg.payload <= 4.095)) {
+                    const obj = {cmd: "setDAC", args: {channel: node.channel, value: msg.payload}};
+                    node.plate.send(obj, (reply) => {
+                        node.value = reply.value;
+                        //node.status({text: node.value});
+                        node.send({payload: node.value});
+                    });
+                } else {
+                    this.warn("invalid DAC value: ignoring");
+                }
+            }else{
+                //TODO: Handle invalid message
             }
         });
 
--- led.js
+++ led.js
@@ -2,24 +2,38 @@ module.exports = function (RED) {
     function LEDNode(config) {
         RED.nodes.createNode(this, config);
         this.plate = RED.nodes.getNode(config.config_plate).plate;
-        var node = this;
-
-        node.on('input', function (msg) {
-            const obj = {
-                cmd: 'setLED',
-                args: { color: msg.payload }
-            };
-
-            const pt = node.plate.plate_type;
-            if (pt == 'THERMO' || pt == 'RELAY') {
-                obj['cmd'] = (msg.payload == 'off' ? "clrLED" : "setLED");
+        const verifier = {cmd: "VERIFY", args: {}};
+        this.plate.send(verifier, (reply) => {
+            var type = RED.nodes.getNode(config.config_plate).model;
+            if (reply.state == 1 && type != "TINKERplate"){
+                this.status({fill: "green", shape: "ring", text: "plate validated"});
+                this.verified = true;
+            }else{
+                this.status({fill: "red", shape: "ring", text: "invalid plate or input"});
+                this.verified = false;
             }
+        });
+        var node = this;
+        node.on('input', function (msg) {
+            if(node.verified){
+                const obj = {
+                    cmd: 'setLED',
+                    args: { color: msg.payload }
+                };
 
-            node.plate.send(obj, (reply) => {
-                node.state = reply.state
-                node.status({ text: node.state });
-                node.send({ payload: node.state });
-            });
+                const pt = node.plate.plate_type;
+                if (pt == 'THERMO' || pt == 'RELAY') {
+                    obj['cmd'] = (msg.payload == 'off' ? "clrLED" : "setLED");
+                }
+
+                node.plate.send(obj, (reply) => {
+                    node.state = reply.state
+                    node.status({ text: node.state });
+                    node.send({ payload: node.state });
+                });
+            }else{
+                //TODO: Handle invalid message
+            }
         });
 
         this.on('close', function () {
