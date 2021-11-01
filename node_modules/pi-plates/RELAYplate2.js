ca45e5ebe5f30c8076e01507b31ac09c872d1168
WallyWare <pi-plates@users.noreply.github.com>
2021-11-01 10:45:41 -0400

    Delete node_modules/pi-plates directory
    Doing it right for Mike

--- node_modules/pi-plates/RELAYplate2.js
+++
@@ -1,9 +0,0 @@
-const BASEplate = require('./BASEplate');
-
-class RELAYplate2 extends BASEplate {
-    constructor (addr) {
-        super(addr, "RELAY2");
-    }
-}
-
-module.exports = RELAYplate2;
